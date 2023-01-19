8b490d3f739acd8227527d769c2bce3bc3f847d6
MasterYoda513 <83755484+MasterYoda513@users.noreply.github.com>
2023-01-19 23:42:27 +0100

    Add files via upload

---
+++ lib/00_journalists.rb
@@ -0,0 +1,188 @@
+def data
+twitterlist = 
+["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+end
+
+def question1
+twitterlist = data
+puts "La liste compte #{twitterlist.length} contacts."
+end
+
+
+def question2
+    twitterlist = data
+    twitterlist.sort_by!(&:length)
+    puts "Le nom le plus court est #{twitterlist[0]}."
+end
+
+def question3
+    twitterlist = data
+    twitterlist.select! {|i| i.length == 6}
+    puts "Il y a #{twitterlist.length} handles qui comptiennent 5 caractères."
+end
+
+def question4
+    twitterlist = data
+    puts "Il y a #{twitterlist.grep(/[UPPERCASE]/).length} handles qui commencent par une majuscules."
+end
+
+def question5
+    twitterlist = data
+    puts "Voici la liste classé par ordre alphabétique."
+    puts
+    sleep 1
+    puts twitterlist.sort
+end
+
+def question6
+    twitterlist = data
+    puts "Voici la liste classé par taille de handle."
+    puts
+    sleep 1
+    puts twitterlist.sort_by!(&:length)
+end
+
+def question7
+    twitterlist = data
+    puts "@epenser est le #{twitterlist.index('@epenser')}e handle dans la liste."
+end
+
+def question8
+    twitter = data
+    twitter.sort_by!(&:length)
+
+    twitter2 = twitter.map do |n| 
+        n.length
+    end
+
+    compteur = 0
+    refBase = twitter2[0]
+    x = twitter2.length
+
+    for i in 0...x
+            if refBase != twitter2[i]
+                puts "Handle size = #{refBase} total = #{compteur}"
+                refBase = twitter2[i]
+                compteur = 0
+            end 
+            compteur += 1
+    end
+    puts "Handle size = #{refBase} total = #{compteur}"
+end
+
+loop do
+    puts
+    puts "=============================================="
+    puts "  BONJOUR ! JE SUIS VOTRE CONSULTANT TWITTER  "
+    puts "=============================================="
+    sleep 1
+    puts
+    puts "Quelle est votre question ?"
+    puts " 1 : Combien y a-t-il de handle dans cette array ?"
+    puts " 2 : Quel est le handle le plus court de cette liste ?"
+    puts " 3 : Combien y-a-t'il de handle contenant 5 caractères ?"
+    puts " 4 : Combien commencent de handle par une majuscule ?"
+    puts " 5 : Trie la liste de handle par ordre alphabétique."
+    puts " 6 : Trie la liste de handle par taille des handle."
+    puts " 7 : Quelle est la position dans l'array de la personne @epenser ?"
+    puts " 8 : Sors-moi une répartition des handle par taille de ces derniers (nombre de handle avec 1 caractère, nombre de handle avec 2 caractères, nombre de handle avec 3 caractères, etc."
+    puts " 0 : Pour partir !"
+    print "> "
+    input = gets.chomp.to_i
+
+    if input == 1
+        puts
+        print "WAIT PLEASE   "
+        60.times do
+            print "/"
+            sleep 0.05
+        end
+        puts puts puts
+        question1
+    
+    elsif input == 2
+        puts
+        print "WAIT PLEASE   "
+        60.times do
+            print "/"
+            sleep 0.05
+        end
+        puts puts puts
+        question2
+    
+    elsif input == 3
+        puts
+        print "WAIT PLEASE   "
+        60.times do
+            print "/"
+            sleep 0.05
+        end
+        puts puts puts
+        question3
+    
+    elsif input == 4
+        puts
+        print "WAIT PLEASE   "
+        60.times do
+            print "/"
+            sleep 0.05
+        end
+        puts puts puts
+        question4
+    
+    elsif input == 5
+        puts
+        print "WAIT PLEASE   "
+        60.times do
+            print "/"
+            sleep 0.05
+        end
+        puts puts puts
+        question5
+
+    elsif input == 6
+        puts
+        print "WAIT PLEASE   "
+        60.times do
+            print "/"
+            sleep 0.05
+        end
+        puts puts puts
+        question6
+
+    elsif input == 7
+        puts
+        print "WAIT PLEASE   "
+        60.times do
+            print "/"
+            sleep 0.05
+        end
+        puts puts puts
+        question7
+
+    elsif input == 8
+        puts
+        print "WAIT PLEASE   "
+        60.times do
+            print "/"
+            sleep 0.05
+        end
+        puts puts puts
+        question8
+    
+    elsif input != 1..8
+        puts
+        puts "============================================="
+        puts "                  AU REVOIR                  "
+        puts "============================================="
+        puts
+        break
+    end
+    puts puts 
+    74.times do 
+        print "#"
+    end
+    puts puts
+    sleep 2
+    
+end
